182f199e11496840e79181792f4aada00cc64814
maimuna-t-333 <maimuna.tabr@gmail.com>
2025-07-02 19:08:34 +0600

    food galary

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -4,6 +4,7 @@ import Review from './Review';
 import HowItWorks from './HowItWorks';
 import FeaturedFood from './FeaturedFood';
 import { AuthContext } from '../../Contexts/AuthContext/AuthContext';
+import FoodGallery from './FoodGallary';
 
     const foodPromise = fetch('https://food-sharing-platform-server.vercel.app/foods').then(res => res.json())
 
@@ -14,7 +15,9 @@ const Home = () => {
         <div className='bg-[#dddbdb]'>
             <Banner></Banner>
             <HowItWorks></HowItWorks>
+            
         <FeaturedFood foodPromise={foodPromise}></FeaturedFood>
+        <FoodGallery></FoodGallery>
             <Review></Review>
         </div>
     );
